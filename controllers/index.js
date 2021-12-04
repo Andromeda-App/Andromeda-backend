3eebeeec0e65f302929cb84ac531b4414944b9b0
wkropat <wkropat@gmail.com>
2021-12-03 19:04:34 -0800

    wsk-routes

--- controllers/index.js
+++ controllers/index.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const userRoutes = require("./userRoutes");
 const eventRoutes = require("./eventRoutes");
 
-router.use(userRoutes)
+router.use("/", userRoutes)
 router.use("/api/events", eventRoutes)
 
 module.exports = router;
